c91a2d7b0d9398068a7eb0aba5e021e3af647d64
Liliia <k.9602585349@gmail.com>
2021-10-29 09:37:45 +0300

    settings_01

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -18,10 +18,9 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['130.193.48.81', 'localhost',]
-
-
-# Application definition
+ALLOWED_HOSTS = ['130.193.48.81',
+    'localhost',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
