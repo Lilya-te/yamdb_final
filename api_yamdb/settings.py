618e50c969aa5db6b39ce8046c05775e31fc208a
Liliia <k.9602585349@gmail.com>
2021-10-31 22:01:40 +0300

    static_0504

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -123,14 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/html/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 print('STATIC_ROOT')
 print(STATIC_ROOT)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 print('STATICFILES_DIRS')
 print(STATICFILES_DIRS)
 
